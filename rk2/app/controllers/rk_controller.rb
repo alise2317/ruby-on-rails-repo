dcf632797a86f5192bc70ee26b63e25f2e195026
Ksenia477 <121382283+Ksenia477@users.noreply.github.com>
2023-11-24 09:51:42 +0000

    Rk

---
+++ rk2/app/controllers/rk_controller.rb
@@ -0,0 +1,7 @@
+class RkController < ApplicationController
+  def input
+  end
+
+  def view
+  end
+end
---
+++ rk2/test/controllers/rk_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class RkControllerTest < ActionDispatch::IntegrationTest
+  test "should get input" do
+    get rk_input_url
+    assert_response :success
+  end
+
+  test "should get view" do
+    get rk_view_url
+    assert_response :success
+  end
+end
