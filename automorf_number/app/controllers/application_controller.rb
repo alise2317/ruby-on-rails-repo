cd79c038b51bffb34adcec9315f3325f25ecd1cc
Ksenia477 <121382283+Ksenia477@users.noreply.github.com>
2023-11-24 09:09:10 +0000

    fin

--- automorf_number/app/controllers/application_controller.rb
+++ automorf_number/app/controllers/application_controller.rb
@@ -1,21 +1,7 @@
 class ApplicationController < ActionController::Base
   def input
   end
+
   def view
-  @result = case params[:op]
-
-    def automorphic_numbers(n)
-      automorphic_nums = []
-
-      (1..n).each do |num|
-        square = num**2
-        num_digits = num.to_s.length
-        last_digits = square.to_s[-num_digits..-1].to_i
-
-        automorphic_nums << num if num == last_digits
-      end
-
-      automorphic_nums
-    end
   end
 end
--- automorf_number/app/controllers/automorf_number_controller.rb
+++ automorf_number/app/controllers/automorf_number_controller.rb
@@ -3,5 +3,21 @@ class AutomorfNumberController < ApplicationController
   end
 
   def view
+    automorphic_nums = []
+    n = params[:val].to_i
+    count = 1
+    (1..n).each do |num|
+      square = num**2
+      num_digits = num.to_s.length
+      last_digits = square.to_s[-num_digits..-1].to_i
+
+      if num == last_digits
+        automorphic_nums << [count, num, square]
+        count += 1
+      end
+    end
+
+    @result = count
+    @solve = automorphic_nums
   end
 end
