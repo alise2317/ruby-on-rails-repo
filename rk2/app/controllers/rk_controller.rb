c1b913eca2cea748d65ed6168aaf16f6d7acbaf5
Ksenia477 <121382283+Ksenia477@users.noreply.github.com>
2023-11-24 18:34:54 +0000

    initial

--- rk2/app/controllers/rk_controller.rb
+++ rk2/app/controllers/rk_controller.rb
@@ -3,5 +3,24 @@
 class RkController < ApplicationController
   def input; end
 
-  def view; end
+  def view
+    arr = params[:val].split.map(&:to_i)
+
+    if arr.empty?
+      flash[:alert] = 'Ошибка. Вы не ввели массив. Попробуйте еще раз'
+      redirect_to action: :input and nil
+    end
+    new_arr = []
+    positive = arr.select(&:positive?)
+    sr_arifm = positive.sum / positive.size.to_f
+    index = arr.size / 2
+    arr[index] = sr_arifm
+
+    arr.each do |element|
+      new_arr << element
+    end
+
+    @before = arr
+    @after = new_arr
+  end
 end
