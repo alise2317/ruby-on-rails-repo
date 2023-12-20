792410e3e796525bda73a928cb3d4e5e63956acc
Ksenia477 <121382283+Ksenia477@users.noreply.github.com>
2023-12-20 19:24:40 +0000

    lab_10

--- lab_10/back/app/controllers/lab_10_controller.rb
+++ lab_10/back/app/controllers/lab_10_controller.rb
@@ -9,7 +9,7 @@ class Lab10Controller < ApplicationController
     @xslt = Nokogiri::XSLT(File.read('transformer.xslt'))
   end
 
-  def index  # почему нигде не используется _code 
+  def index  
     reverse_proxy('http://127.0.0.1:3001/') do |config|  # работа с сервером
       config.on_success do |_code, response|   # если успешно, то
         if request.format == 'application/xml'  # если формат = application, то вернет без изменений
