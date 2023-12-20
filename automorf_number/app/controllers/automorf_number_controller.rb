dcb25665c9500abbcaaf291d65c27b07d1c8ddd6
Ksenia477 <121382283+Ksenia477@users.noreply.github.com>
2023-12-20 19:24:15 +0000

    automorf

--- automorf_number/app/controllers/automorf_number_controller.rb
+++ automorf_number/app/controllers/automorf_number_controller.rb
@@ -12,20 +12,33 @@ class AutomorfNumberController < ApplicationController
   end
 
   def view
-    automorphic_nums = []
-    n = params[:val].to_i
-      count = 0
-      (1..n).each do |num|
-        square = num**2
-        num_digits = num.to_s.length
-        last_digits = square.to_s[-num_digits..-1].to_i
+    unless params[:val].to_i.to_s == params[:val]
+    then
+        @result = "Введите корректное значение"
+        @solve = [[0,0,0]]
+    else
+      if params[:val].to_i >= 1000 then @result = "Введите значение меньше 1000"
+        @solve = [[0, 0, 0]]
+      else
+        if params[:val].to_i < 0 then @result = "Введите значение больше 0"
+        else
+          automorphic_nums = []
+          n = params[:val].to_i
+          count = 0
+          (1..n).each do |num|
+            square = num**2
+            num_digits = num.to_s.length
+            last_digits = square.to_s[-num_digits..-1].to_i
 
-        if num == last_digits
-          automorphic_nums << [count, num, square]
-          count += 1
+            if num == last_digits
+              automorphic_nums << [count, num, square]
+              count += 1
+            end
+          end
+        @result = count
+        @solve = automorphic_nums
         end
       end
-    @result = count
-    @solve = automorphic_nums
+    end
   end
 end
